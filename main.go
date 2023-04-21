87963dd443c0b3b282ff3fbdd2692796a5fce43c
Adam Reese <adam@reese.io>
2023-04-21 13:44:27 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	spinhttp "github.com/fermyon/spin/sdk/go/http"
+	"github.com/fermyon/spin/sdk/go/key_value"
+	"github.com/google/go-github/v51/github"
+)
+
+func init() {
+	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
+		store, err := key_value.Open("default")
+		if err != nil {
+			log.Print(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer key_value.Close(store)
+
+		log.Print(key_value.GetKeys(store))
+
+		webhookSecretKey, err := key_value.Get(store, "webhookSecretKey")
+		if err != nil {
+			log.Print(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		payload, err := github.ValidatePayload(r, webhookSecretKey)
+		if err != nil {
+			log.Print(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		switch github.WebHookType(r) {
+		case "ping":
+			log.Print("ping")
+			w.WriteHeader(http.StatusOK)
+		case "push":
+			var event github.PushEvent
+			if err := json.Unmarshal(payload, &event); err != nil {
+				log.Print(err)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			log.Printf("%v", event)
+		}
+	})
+}
+
+func main() {}
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+set -x
+
+payload_file="${1:-testdata/ping.json}"
+event="${2:-ping}"
+
+payload=$(<"${payload_file}")
+
+webhook_secret="$(<secret.txt)"
+
+WEBHOOK_SIGNATURE=$(echo -n "$payload" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
+WEBHOOK_SIGNATURE_256=$(echo -n "$payload" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr -d '\n')
+
+curl \
+  --header "X-GitHub-Event: ping" \
+  --header "X-GitHub-Delivery: nil" \
+  --header "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
+  --header "X-Hub-Signature-256: sha256=$WEBHOOK_SIGNATURE_256" \
+  --data "$payload" \
+  -L \
+    http://gh-events-ar.fermyon.app
