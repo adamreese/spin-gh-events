10a68384e021536331af737becc405dd7b4b8760
Adam Reese <adam@reese.io>
2023-04-21 14:03:44 -0700

    clean up test script

--- test.sh
+++ test.sh
@@ -11,14 +11,14 @@ payload=$(<"${payload_file}")
 
 webhook_secret="$(<secret.txt)"
 
-WEBHOOK_SIGNATURE=$(echo -n "$payload" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
-WEBHOOK_SIGNATURE_256=$(echo -n "$payload" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr -d '\n')
+webhook_signature=$(echo -n "$payload" | openssl sha1 -hmac "$webhook_secret" -binary | xxd -p)
+webhook_signature_256=$(echo -n "$payload" | openssl dgst -sha256 -hmac "$webhook_secret" -binary | xxd -p |tr -d '\n')
 
 curl \
   --header "X-GitHub-Event: ping" \
   --header "X-GitHub-Delivery: nil" \
-  --header "X-Hub-Signature: sha1=$WEBHOOK_SIGNATURE" \
-  --header "X-Hub-Signature-256: sha256=$WEBHOOK_SIGNATURE_256" \
+  --header "X-Hub-Signature: sha1=$webhook_signature" \
+  --header "X-Hub-Signature-256: sha256=$webhook_signature_256" \
   --data "$payload" \
   -L \
-    http://gh-events-ar.fermyon.app
+    "${endpoint}"
